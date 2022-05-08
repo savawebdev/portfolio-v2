f6bf69ff8be2d4abee1f77a7d31c1a0d3b21c6ff
savawedev <andrei.sava.dev@gmail.com>
2022-05-08 16:23:38 +0300

    Added logic to the Explore button

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// Toggle show mobile nav menu
 const navBtn = document.querySelector(".masthead__nav-btn");
 const navMenu = document.querySelector(".masthead__nav");
 
 navBtn.addEventListener("click", () => {
   navMenu.classList.toggle("masthead__nav--show");
+});
+
+// Event for the Explore button
+const exploreBtn = document.querySelector(".hero__btn");
+
+exploreBtn.addEventListener("click", () => {
+  window.location = "#aboutme";
 });
